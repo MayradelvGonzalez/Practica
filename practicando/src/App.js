9aac26a9d36c823ef394bc4c7d31fec70831ff31
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-07-31 22:28:04 -0300

    practiando/public/index.html

--- practicando/src/App.js
+++ practicando/src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+import planetaTierra from './assets/planetaTierra.jpg' ;
+import videoNave from './assets/videoNave.mp4';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={planetaTierra} className="App-logo" alt="logo" />
+       
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <video src={videoNave} className="video"></video> 
         </a>
       </header>
     </div>
